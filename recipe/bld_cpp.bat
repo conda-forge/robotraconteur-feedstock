8a2a4d41463e339d989335015a46dbe4642e93fd
John Wason <wason@wasontech.com>
2025-07-17 01:56:17 -0400

    work

--- recipe/bld_cpp.bat
+++ recipe/bld_cpp.bat
@@ -5,6 +5,7 @@ cd build2
 if %errorlevel% neq 0 exit /b %errorlevel%
 cmake -GNinja -DBUILD_TESTING=OFF -DBUILD_DOCUMENTATION=OFF -DBUILD_GEN=ON ^
   -DCMAKE_BUILD_TYPE:STRING=Release -DBUILD_SHARED_LIBS:BOOL=ON ^
+  -DCMAKE_VERBOSE_MAKEFILE=ON ^
   %CMAKE_ARGS% ^
   ..
 if %errorlevel% neq 0 exit /b %errorlevel%
