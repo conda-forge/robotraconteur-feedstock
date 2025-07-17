919e119fe34458e9a8d029cb140b8d6df9875fd1
John Wason <wason@wasontech.com>
2025-07-17 00:57:11 -0400

    Initial update to split cpp and py packages

--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -11,13 +11,12 @@ fi
 mkdir -p build2
 cd build2
 
-cmake -DBUILD_TESTING=OFF -DBUILD_GEN=ON -DBUILD_PYTHON3=ON \
-    -DBUILD_NET=ON -DRR_NET_BUILD_NATIVE_ONLY=ON \
-    -DRR_NET_INSTALL_NATIVE_LIB=ON -DBUILD_DOCUMENTATION=OFF \
+cmake -DBUILD_TESTING=OFF -DBUILD_GEN=OFF -DBUILD_PYTHON3=ON \
+    -DBUILD_CORE=OFF \
+    -DBUILD_DOCUMENTATION=OFF \
     -DCMAKE_BUILD_TYPE:STRING=Release -DPYTHON3_EXECUTABLE=$PREFIX/bin/python \
     -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_SHARED_LIBS:BOOL=ON  ${CMAKE_ARGS} ..
 
 cmake --build . --config Release -- -j$CPU_COUNT
-cmake --build . --config Release --target install
 cd out/Python3
 $PYTHON -m pip install --no-deps --ignore-installed . -vv
