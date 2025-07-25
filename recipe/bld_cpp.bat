ec5d15cebcd760dca88e937579ce27ee5f78eab4
John Wason <wason@wasontech.com>
2025-07-25 19:06:09 -0400

    Improve cmake arguments

--- recipe/bld_cpp.bat
+++ recipe/bld_cpp.bat
@@ -4,7 +4,7 @@ md build2
 cd build2
 if %errorlevel% neq 0 exit /b %errorlevel%
 cmake -GNinja -DBUILD_TESTING=OFF -DBUILD_DOCUMENTATION=OFF -DBUILD_GEN=ON ^
-  -DCMAKE_BUILD_TYPE:STRING=Release -DBUILD_SHARED_LIBS:BOOL=ON ^
+  -DBUILD_SHARED_LIBS:BOOL=ON ^
   -DCMAKE_VERBOSE_MAKEFILE=ON ^
   %CMAKE_ARGS% ^
   ..
